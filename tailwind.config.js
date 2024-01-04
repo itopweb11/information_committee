980cd97a75e9a15515de595a469e6cd58b6c4e5c
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2024-01-05 00:55:01 +0300

    Start of the projectÐ

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,24 @@ module.exports = {
   content: ["./src/**/*.{html,js,php}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'boxShadow': '0 5px 40px 0 rgba(1, 3, 8, 0.10)',
+        'boxShadowBtn': '0px 5px 40px 0px rgba(1, 3, 8, 0.10)',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(circle, var(--tw-gradient-stops))',
+        'custom-gradient': 'linear-gradient(180deg, #0B162A 0%, #010308 100%)',
+        'backgroundText': 'rgba(8, 108, 128, 0.10)',
+      },
+      colors: {
+        'custom-blue': '#0B162A',
+        'custom-dark': '#010308',
+      },
+      background: {
+        'backgroundMenu': 'rgba(255, 255, 255, 0.04)'
+      },
+    },
   },
   plugins: plugins,
 };
